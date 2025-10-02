7e2293dddf91a5c236ff2fd106d0cfab9a23b1bf
Alexeyzem <Alex.zem2015@mail.ru>
2025-10-02 15:11:10 +0300

    fix

--- students_folder/zemliakov_alexey/lab_2/main.py
+++ students_folder/zemliakov_alexey/lab_2/main.py
@@ -1,39 +1,44 @@
 import yaml
 import logging
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, model_validator, ConfigDict
 from typing import Optional, Literal, List
 
 from typing_extensions import Self
 
 
+russian_regular = r'^[а-яА-ЯёЁ\s]+$'
+
 class UserSpec(BaseModel):
     user_id: int
-    username: str = Field(min_length=1, pattern=r'^[а-яА-ЯёЁ\s]+$')
-    surname: str = Field(min_length=1, pattern=r'^[а-яА-ЯёЁ\s]+$')
-    second_name: Optional[str] = Field(pattern=r'^[а-яА-ЯёЁ\s]+$', default='')
+    username: str = Field(min_length=1, pattern=russian_regular)
+    surname: str = Field(min_length=1, pattern=russian_regular)
+    second_name: Optional[str] = Field(pattern=russian_regular, default='')
     email: str = (
         Field(pattern=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'))
     status: Literal['active', 'non-active']
+    model_config = ConfigDict(extra='forbid')
 
 
 class ProfileSpec(UserSpec):
-    bio: str = Field(min_length=1, pattern=r'^[а-яА-ЯёЁ\s]+$')
+    bio: str = Field(min_length=1, pattern=russian_regular)
     url: str = Field(pattern=r'://')
 
 
 class ItemSpec(BaseModel):
     item_id: int
-    name: str = Field(min_length=1, pattern=r'^[а-яА-ЯёЁ\s]+$')
-    desc: str = Field(min_length=1, pattern=r'^[а-яА-ЯёЁ\s]+$')
+    name: str = Field(min_length=1, pattern=russian_regular)
+    desc: str = Field(min_length=1, pattern=russian_regular)
     price: float = Field(gt=0)
+    model_config = ConfigDict(extra='forbid')
 
 
 class ServiceSpec(BaseModel):
     service_id: int
-    name: str = Field(min_length=1, pattern=r'^[а-яА-ЯёЁ\s]+$')
-    desc: str = Field(min_length=1, pattern=r'^[а-яА-ЯёЁ\s]+$')
+    name: str = Field(min_length=1, pattern=russian_regular)
+    desc: str = Field(min_length=1, pattern=russian_regular)
     price: float = Field(gt=0)
+    model_config = ConfigDict(extra='forbid')
 
 
 class OrderLineSpec(BaseModel):
@@ -42,6 +47,7 @@ class OrderLineSpec(BaseModel):
     item_line: ItemSpec
     quantity: float = Field(gt=0)
     line_price: Optional[float] = Field(gt=0, default=None)
+    model_config = ConfigDict(extra='forbid')
 
     @model_validator(mode="after")
     def calculate_line_prices(self) -> Self:
@@ -53,13 +59,15 @@ class OrderSpec(BaseModel):
     order_id: int
     user_info: ProfileSpec
     order_lines: List[OrderLineSpec]
+    model_config = ConfigDict(extra='forbid')
 
 
 class OrdersSpec(BaseModel):
     market_place_orders: List[OrderSpec]
+    model_config = ConfigDict(extra='forbid')
 
 
-def get_orders_from_yaml(yaml_data) -> OrdersSpec | None:
+def get_orders_from_yaml(yaml_data: dict) -> OrdersSpec | None:
     if yaml_data and 'market_place_orders' in yaml_data:
         market_place_orders = yaml_data['market_place_orders']
         return OrdersSpec(market_place_orders=market_place_orders)
@@ -73,9 +81,9 @@ try:
         yaml_data = yaml.safe_load(file)
         res = get_orders_from_yaml(yaml_data)
 except FileNotFoundError:
-    print('File not found')
+    logging.error('File not found')
 except Exception as e:
-    print(f'Произошла ошибка при чтении файла {e}')
+    logging.error(f'Произошла ошибка при чтении файла: {e}')
 
 logging.info(f'Orders spec: {res}', exc_info=True)
 logging.info(f'Order spec first: {res.market_place_orders[0]}', exc_info=True)
