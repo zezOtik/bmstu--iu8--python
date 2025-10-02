af6d1809347133d8cc045dbbb50f434c0d0a9a39
Alexeyzem <Alex.zem2015@mail.ru>
2025-10-02 16:47:30 +0300

    add some test

---
+++ tests/Zemliakov_Alexey/lab_3/test_user_spec.py
@@ -0,0 +1,30 @@
+import logging
+
+import pytest
+from pydantic import ValidationError
+
+from students_folder.zemliakov_alexey.lab_2.main import UserSpec
+
+logger = logging.getLogger("test_Profile")
+
+
+@pytest.mark.zmv_lab1
+def test_class_profile(yaml_test_data):
+    test_cases = yaml_test_data("Zemliakov/lab_3/UserSpec.yaml")
+    for test_desc, value, answer in test_cases:
+        logger.info(f"Testing: {test_desc}")
+        try:
+            test_class = UserSpec.model_validate(value)
+            # Ожидаем answer = True
+            test_answer = True
+            assert (
+                answer == test_answer
+            ), f"Expected validation to fail for {test_desc}, but it passed"
+            logger.info(f"{test_desc} PASSED - validation succeeded as expected")
+        except ValidationError as e:
+            # Ожидаем answer = False
+            test_answer = False
+            assert (
+                answer == test_answer
+            ), f"Expected validation to pass for {test_desc}, but got error: {e}"
+            logger.info(f"{test_desc} PASSED - validation failed as expected: {e}")
