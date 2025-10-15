5a3846c9c5bced27c707b5c1df3649c1259f7736
zlatanikqwede <zlatanikqwed@gmail.com>
2025-10-15 14:50:55 +0300

    lab3 reresolve conflicts
    :wq
    :wq

---
+++ tests/Nikulina/lab_3/test_ProfileSpec.py
@@ -0,0 +1,27 @@
+import logging
+import pytest
+from pydantic import ValidationError
+
+from students_folder.Nikulina.lab2.lab_2 import ProfileSpec
+
+logger = logging.getLogger("test_ProfileSpec")
+
+
+@pytest.mark.nze_lab3
+def test_class_profile(yaml_test_data):
+    test_cases = yaml_test_data("Nikulina/lab_3/Profile.yaml")
+    for test_desc, value, answer in test_cases:
+        logger.info(f"Testing: {test_desc}")
+        try:
+            test_class = ProfileSpec.model_validate(value)
+            test_answer = True
+            assert (
+                answer == test_answer
+            ), f"Expected validation to fail for {test_desc}, but it passed"
+            logger.info(f"{test_desc} PASSED - validation succeeded as expected")
+        except ValidationError as e:
+            test_answer = False
+            assert (
+                answer == test_answer
+            ), f"Expected validation to pass for {test_desc}, but got error: {e}"
+            logger.info(f"{test_desc} PASSED - validation failed as expected: {e}")
